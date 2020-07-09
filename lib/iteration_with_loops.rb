5d01201cc7d6130411508aa5281269faa5a95ec5
Elliott Stein <estein1988@gmail.com>
2020-07-09 15:44:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,13 +1,13 @@
 def find_even_values(src)
-  integers = [
+  src = [
     [10, 11], 
     [99, 50, 3, 4], 
     [23, 41] 
     ]
   row_index = 0 
-    while row_index < integers.count do 
+    while row_index < src.count do 
       element_index = 0 
-        while element_index < integers[row_index].count do 
+        while element_index < src[row_index].count do 
         p integers[row_index][element_index] if element_index % 2 == 0 
   end 
   row_index +=1
